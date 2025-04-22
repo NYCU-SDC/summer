54dd7bdf8bbf0bf20efcf7a9aa81620eb3a96965
Kyle9410-Chen <kyle941010@gmail.com>
2025-04-22 20:21:49 +0800

    feat: add config.go to merge the configs of different level

---
+++ pkg/config/config.go
@@ -0,0 +1,35 @@
+package config
+
+import (
+	"errors"
+	"reflect"
+)
+
+func Merge(base, override *any) (*any, error) {
+	if base == nil {
+		return nil, errors.New("base config cannot be nil")
+	}
+	if override == nil {
+		return base, nil
+	}
+
+	final := *base
+	baseVal := reflect.ValueOf(&final).Elem()
+	overrideVal := reflect.ValueOf(override).Elem()
+
+	if baseVal.Type() != overrideVal.Type() {
+		return nil, errors.New("config types do not match")
+	}
+
+	for i := 0; i < baseVal.NumField(); i++ {
+		field := baseVal.Field(i)
+		overrideField := overrideVal.Field(i)
+		zero := reflect.Zero(field.Type()).Interface()
+
+		if field.CanSet() && !reflect.DeepEqual(overrideField.Interface(), zero) {
+			field.Set(overrideField)
+		}
+	}
+
+	return &final, nil
+}
