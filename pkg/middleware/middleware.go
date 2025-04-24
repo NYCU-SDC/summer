2af0b317e845118689d55a804b4b4e4359b0cb52
yukicoder0509 <bendoris0509@gmail.com>
2025-04-24 16:39:33 +0800

    feat: add comment on Append and HandlerFunc method in  middleware.go to explain the funtion

--- pkg/middleware/middleware.go
+++ pkg/middleware/middleware.go
@@ -12,6 +12,7 @@ func NewSet(middlewares ...func(next http.HandlerFunc) http.HandlerFunc) *Set {
 	return &Set{middlewares: middlewares}
 }
 
+// Append return a new Set with the new middleware added to the end of the chain, won't change the original one
 func (s *Set) Append(middleware func(next http.HandlerFunc) http.HandlerFunc) Set {
 	newMiddlewares := make([]func(next http.HandlerFunc) http.HandlerFunc, len(s.middlewares))
 	copy(newMiddlewares, s.middlewares)
@@ -20,6 +21,7 @@ func (s *Set) Append(middleware func(next http.HandlerFunc) http.HandlerFunc) Se
 	return Set{middlewares: newMiddlewares}
 }
 
+// HandlerFunc returns a http.HandlerFunc that applies all middlewares in the set in the appended order to the given handler
 func (s *Set) HandlerFunc(next http.HandlerFunc) http.HandlerFunc {
 	for i := len(s.middlewares) - 1; i >= 0; i-- {
 		next = s.middlewares[i](next)
