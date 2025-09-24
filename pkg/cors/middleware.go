92059e2885400ab3a0bce312a4c531204faad105
mochizuki <1p41p4jejo@gmail.com>
2025-09-24 21:56:24 +0800

    fix: update CORS middleware to set Access-Control-Allow-Origin based on request origin

--- pkg/cors/middleware.go
+++ pkg/cors/middleware.go
@@ -1,9 +1,10 @@
 package cors
 
 import (
-	"go.uber.org/zap"
 	"net/http"
 	"slices"
+
+	"go.uber.org/zap"
 )
 
 func CORSMiddleware(next http.HandlerFunc, logger *zap.Logger, allowOrigin []string) http.HandlerFunc {
@@ -16,7 +17,7 @@ func CORSMiddleware(next http.HandlerFunc, logger *zap.Logger, allowOrigin []str
 		}
 
 		if slices.Contains(allowOrigin, "*") {
-			w.Header().Set("Access-Control-Allow-Origin", "*")
+			w.Header().Set("Access-Control-Allow-Origin", origin)
 		} else if slices.Contains(allowOrigin, origin) {
 			w.Header().Set("Access-Control-Allow-Origin", origin)
 			w.Header().Set("Access-Control-Allow-Credentials", "true")
